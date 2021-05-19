f5a5101bc3390b5fdb3dbd50bcdbdfce95c23b84
Rodrigo Munguía <rmg.jp@outlook.com>
2021-05-19 14:02:49 -0500

    Agregado el metodo de guardar y buscar actividades.

---
+++ controllers/actividad-controller.js
@@ -0,0 +1,61 @@
+const Sequelize = require('sequelize');
+const Models = require('../models');
+
+const getAllTareas = async (idGrupo)=>{
+    try{
+        const tareas = Models.tareas.findAll({
+
+        })
+    }
+    catch (err){
+
+    }
+}
+
+const getAllTareasByTema = async (idTema)=>{
+    try{
+        const tareas = await Models.tarea.findAll({
+            where:{
+                idtema: idTema
+            }
+        });
+        return tareas;
+    }
+    catch (err){
+        console.log(err)
+    }
+}
+
+const guardarDesdeGrid = async (req, res, idtema) => {
+    //Se obtiene el arreglo alojado en objeto invisible del body correspondiente a la tabla
+    tareas = JSON.parse(req.body.valorTabla);
+    console.log("Tareas leidos: " + {tareas});
+    //Ciclo para iterar entre los datos del arreglo Tabla
+
+    for (let tarea in tareas) {
+        //Llamado del modelo para buscar el registro
+        //si existe el registro no se guarda
+        //si no existe el registro se guarda
+        //evita duplicados.
+        await Models.tarea.create(
+            {
+                idtema: idtema,
+                nombre: tareas[tarea].nombreCol,
+                valor: tareas[tarea].valorCol,
+                tipo: tareas[tarea].tipoCol,
+            });
+    }
+};
+
+const guardarTarea = async (req, res) => {
+
+};
+
+
+module.exports = {
+    guardarTarea,
+    guardarDesdeGrid,
+    getAllTareasByTema,
+    getAllTareas
+
+}
--- routes/actividad-rutas.js
+++ routes/actividad-rutas.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const temaController = require('../controllers/tema-controller');
+const actividadController = require('../controllers/actividad-controller');
+
+
+router.get('/grupo/actividades/:idgrupo', async (req,res)=>{
+    //Metodo para buscar Temas
+    const temas = await temaController.getTemasByGrupoEtiquetas(req.params.idgrupo);
+    //Metodo para buscar Actividades
+
+    //Renderización de la vista.
+    res.render('grupo/vista-grupo-actividades', {idgrupo:req.params.idgrupo, temas});
+});
+
+router.get('/grupo/actividades/:idgrupo/:idtema', async (req,res)=>{
+    //Metodo para buscar Temas
+    const temas = await temaController.getTemasByGrupoEtiquetas(req.params.idgrupo);
+    //Metodo para buscar Actividades
+    const actividades = await actividadController.getAllTareasByTema(req.params.idtema);
+    //Renderización de la vista.
+    res.render('grupo/vista-grupo-actividades', {idgrupo:req.params.idgrupo, temas, actividades});
+});
 
 module.exports = router;
