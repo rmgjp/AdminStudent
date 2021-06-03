4367418735f1db382732ad0f6d27e544dd06b1ce
Rodrigo Munguía <rmg.jp@outlook.com>
2021-06-03 13:00:43 -0500

    Agregado soporte para selección de archivos para importación de listas.

--- routes/grupos-rutas.js
+++ routes/grupos-rutas.js
@@ -21,27 +21,35 @@ router.get('/grupo/wizard-crear-grupo-manual', ((req,res)=>
 ));
 router.post('/grupo/wizard-crear-grupo-manual', controladorGrupo.createGrupoManual);
 
-//Ruta para importar archivo excel
-router.get('/grupo/wizard-crear-grupo-importado', ((req,res)=>
+//Rutas para importar archivo excel
+
+router.post('/grupo/importar-archivo', load.single('archivo'), async (req,res) =>{
+    res.redirect('/');
+});
+
+router.get('/grupo/wizard-crear-grupo-importado',((req,res)=>
         res.render('grupo/datosgrupo')
 ));
 
+//Rutas de Edición del grupo
 router.get('/grupo/editar/:idgrupo',  controladorGrupo.getDatosGrupoEditar);
 
 router.put('/grupo/editar/:idgrupo', controladorGrupo.editarGrupo);
 
-
+//Ruta para vista inicial del grupo.
 router.get('/grupo-inicio/:idgrupo', controladorGrupo.getDatosGrupo);
 
+//Ruta para eliminar el grupo cuando se cancela el Wizard.
 router.get('/abortar-grupo/:idgrupo', controladorGrupo.abortarGrupo);
 
+//Ruta para mover el grupo a la papelera de reciclaje.
 router.put('/moverapapelera/:idgrupo', controladorGrupo.moveraPapelera);
 
+//Ruta para sacar el grupo de la papelera de reciclaje.
 router.put('/restaurargrupo/:idgrupo',controladorGrupo.restaurarGrupo);
 
+//Ruta para eliminar el grupo junto con los datos asociados a el.
 router.delete('/eliminargrupo/:idgrupo',controladorGrupo.eliminarGrupo);
-
-
 
 //router.post('/crearusuario/manual', controladorGrupo.create);
 module.exports = router;
