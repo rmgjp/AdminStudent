f0ef4526a7f39f418e4c453f66d8bc1a8e320e61
violeta <viole.vera.salazar@gmail.com>
2021-06-24 12:03:14 -0500

    Helper para calificar individualmente

---
+++ helpers/grid-calificacion-individual-helper.js
@@ -0,0 +1,29 @@
+var table = $('#table')
+var tablaDatos;
+var i = 0;
+
+
+$(function cargarDatos(){
+    var lista = document.getElementById("valorTabla").value;
+    lista = JSON.parse(lista);
+
+    if(lista){
+        console.log("Lista encontrada.")
+        var rows= [];
+        for(let alumno= 0; alumno < lista.length; alumno++){
+            rows.push(lista[alumno]);
+        }
+        console.log(rows);
+        table.bootstrapTable('load', rows);
+        i = lista.length;
+        tablaDatos = table.bootstrapTable('getData');
+        document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
+    }
+})
+
+
+$('#table').on('editable-save.bs.table', function(e, field, row, oldValue, $el){
+    tablaDatos = table.bootstrapTable('getData');
+    document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
+    console.log(tablaDatos);
+})
