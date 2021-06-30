4b141e5927d5f23ba870429e49eb7a53c21c8ec8
Rodrigo Munguía <rmg.jp@outlook.com>
2021-06-30 18:23:19 -0500

    Misc.

--- helpers/vista-calificaciones-individual-helper.js
+++ helpers/vista-calificaciones-individual-helper.js
@@ -10,8 +10,8 @@ $(function cargarDatos(){
     if(lista){
         console.log("Lista encontrada.")
         var rows= [];
-        for(let alumno= 0; alumno < lista.length; alumno++){
-            rows.push(lista[alumno]);
+        for(let dato = 0; dato < lista.length; dato++){
+            rows.push(lista[dato]);
         }
         table.bootstrapTable('load', rows);
         i = lista.length;
