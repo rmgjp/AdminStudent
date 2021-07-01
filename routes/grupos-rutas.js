fdda35e4d4189831156f8185c6fa8e66ab96c783
Rodrigo Munguía <rmg.jp@outlook.com>
2021-07-01 18:29:14 -0500

    Misc

--- routes/grupos-rutas.js
+++ routes/grupos-rutas.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const controladorGrupo = require('../controllers/grupo-controller')
-const fileReader = require('../helpers/lector-archivos-helper')
+
 
 router.use(function (req, res, next) {
     next();
