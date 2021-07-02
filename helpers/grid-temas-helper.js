107598e593d38302dc09fbd06ceb9623dd5c1d73
Rodrigo Munguía <rmg.jp@outlook.com>
2021-07-02 10:11:09 -0500

    Agregado helper para importación de temas.

---
+++ helpers/grid-temas-helper.js
@@ -0,0 +1,63 @@
+var table = $('#table')
+var tablaDatos;
+var i = 0;
+
+$(function cargarDatos(){
+    var lista = document.getElementById("valorTabla").value;
+    lista = JSON.parse(lista);
+
+    if(lista){
+        console.log("Lista encontrada.")
+        var rows= [];
+        for(let dato = 0; dato < lista.length; dato++){
+            rows.push(lista[dato]);
+        }
+        table.bootstrapTable('load', rows);
+        i = lista.length;
+        tablaDatos = table.bootstrapTable('getData');
+        console.log(tablaDatos);
+        document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
+        document.getElementById("numerotema").value =  i + 1;
+    }
+})
+
+$('#table').on('editable-save.bs.table', function(e, field, row, oldValue, $el){
+    tablaDatos = table.bootstrapTable('getData');
+    document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
+    console.log(tablaDatos);
+})
+
+
+$('#add').on('click', function () {
+    if(document.getElementById('numerotema').value && document.getElementById('nombre').value){
+
+        table.bootstrapTable('insertRow',{
+            index: i,
+            row:{
+                numerotema:  document.getElementById('numerotema').value,
+                nombre: document.getElementById('nombre').value,
+            }
+        });
+
+        document.getElementById("nombre").value = "";
+        tablaDatos = table.bootstrapTable('getData');
+        document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
+        i = i + 1
+        document.getElementById("numerotema").value = i+1;
+        console.log(tablaDatos);
+    }
+})
+
+$('#remove').on('click', function () {
+    var claves = $.map(table.bootstrapTable('getSelections'), function (row){
+        return row.numerotema
+    })
+    table.bootstrapTable('remove',{
+        field: 'numerotema',
+        values: claves
+    })
+    tablaDatos = table.bootstrapTable('getData');
+    document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
+    i = i - 1
+    document.getElementById("numerotema").value =  i + 1;
+})
