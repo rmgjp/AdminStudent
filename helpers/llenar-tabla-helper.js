f325f6ba8fe9728cae86d9968f3b08f4623e1b8a
Violeta Saraí Vera Salazar <viole.vera.salazar@gmail.com>
2021-07-22 13:06:46 -0500

    Crea script para rellenar las tablas en la vista de crear nuevo equipo

--- helpers/llenar-tabla-helper.js
+++ helpers/llenar-tabla-helper.js
@@ -1,31 +1,31 @@
 let table = $('#table');
 let tablaDatos;
 let i = 0;
-var selectedIndex;
 
-
-$(function cargarDatos(){
+$(function cargarDatos() {
     var lista = document.getElementById("valorTabla").value;
     lista = JSON.parse(lista);
 
-    if(lista){
-        console.log("Lista encontrada.")
-        var rows= [];
-        for(let dato = 0; dato < lista.length; dato++){
+    if (lista){
+        var rows = [];
+        for (let dato = 0; dato < lista.length; dato++) {
             rows.push(lista[dato]);
         }
         table.bootstrapTable('load', rows);
         i = lista.length;
         tablaDatos = table.bootstrapTable('getData');
-        console.log(tablaDatos);
         document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
     }
 })
 
-$('#table').on('editable-save.bs.table', function(e, field, row, oldValue, $el){
+table.on('editable-save.bs.table', function (e, field, row, oldValue, $el) {
     tablaDatos = table.bootstrapTable('getData');
-    document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
-
+    document.getElementById("valorTabla").value = JSON.stringify(tablaDatos)
+    var $next = $(this).closest('tr').next().find('.editable');
+    setTimeout(function() {
+        $next.editable('show');
+    }, );
+    console.log($next);
 })
 
 
