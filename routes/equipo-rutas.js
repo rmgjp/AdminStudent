78fa14072140166c6e05085fc75dc619ba4f91e2
Violeta Saraí Vera Salazar <viole.vera.salazar@gmail.com>
2021-07-23 12:50:44 -0500

    Ruta para guardar datos de equipo

--- routes/equipo-rutas.js
+++ routes/equipo-rutas.js
@@ -5,6 +5,6 @@ const equipoController = require('../controllers/equipo-controller');
 router.get('/grupo/equipos/:idgrupo',equipoController.renderAllTeams);
 
 router.get('/equipo/nuevo/:idgrupo', equipoController.renderNewTeam);
-
+router.post('/equipo/nuevo/:idgrupo', equipoController.saveTeam)
 
 module.exports = router;
