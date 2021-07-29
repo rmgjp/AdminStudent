047b5aefec4f35c60b292d38b2762ac195819eab
Rodrigo Munguía <rmg.jp@outlook.com>
2021-07-29 12:02:20 -0500

    Agregado soporte para creación y consulta de equipos en un grupo.

--- controllers/equipo-controller.js
+++ controllers/equipo-controller.js
@@ -6,6 +6,29 @@ const renderAllTeams = async (req, res) => {
     res.render('equipo/vista-grupo-equipo', {idgrupo: req.params.idgrupo})
 }
 
+const renderSelectedTeams = async (req, res) => {
+    const equipo = await Models.equipo.findOne({where: {id: req.params.idequipo}});
+    const temas = await Models.tema.findAll({
+        include:[{
+            model : Models.equipotema,
+            where:{
+                idequipo: req.params.idequipo
+            }
+        }]
+    })
+    const alumnos = await Models.alumno.findAll({
+        include: [{
+            model : Models.alumnoequipo,
+            where:{idequipo: req.params.idequipo}
+        }]
+    })
+    console.log({alumnos});
+    const equipos = await Models.equipo.findAll({where: {idgrupo: req.params.idgrupo}});
+
+
+    res.render('equipo/vista-grupo-equipo', {idgrupo: req.params.idgrupo, equipo, equipos,listaFormateadaAlumnos: JSON.stringify(alumnos), listaFormateadaTemas: JSON.stringify(temas)})
+}
+
 const renderNewTeam = async (req, res) => {
     let listaFormateadaTemas = [];
     let listaFormateadaAlumnos = [];
@@ -24,30 +47,51 @@ const renderNewTeam = async (req, res) => {
 
     res.render('equipo/equipo-nuevo', {listaFormateadaAlumnos, listaFormateadaTemas, idgrupo: req.params.idgrupo});
 }
-const saveTeam = async (req,res) => {
-    const nombre = req.params.body.nombreBox;
-    const listaTemas = req.body.listaTemas;
-    const listaAlumnos = req.body.listaAlumnos;
-    let error = [];
-    if(!listaTemas){
-        error.push({text: 'No se ha seleccionado ningún tema.'});
-        res.render('equipo/equipo-nuevo', {listaFormateadaAlumnos: req.body.valorTablaTemas, listaFormateadaTemas:req.body.valorTablaAlumnos, idgrupo:req.params.idgrupo})
-    }
-    else if(!listaAlumnos){
-        error.push({text: 'No se ha seleccionado ningún alumno.'});
-    }
-    else if(!listaAlumnos && !listaTemas){
-        error.push({text: 'No se ha seleccionado ningún tema.'});
-        error.push({text: 'No se ha seleccionado ningún alumno.'});
-    }
-    else {
-        await Models.equipo.create({
+const saveTeam = async (req, res) => {
+    const nombre = req.body.nombreBox;
+    const listaTemas = tryParseJSON(req.body.listaTemas);
+    const listaAlumnos = tryParseJSON(req.body.listaAlumnos);
+    let errors = [];
+    if (!listaTemas || !listaAlumnos) {
+        //const listaFormateadaAlumnos = req.body.valorTablaAlumnos.value;
+        //const listaFormateadaTemas = req.body.valorTablaTemas.value;
+        if (!listaTemas) {
+            errors.push({text: 'No se ha seleccionado ningún tema.'});
+        }
+        if (!listaAlumnos) {
+            errors.push({text: 'No se ha seleccionado ningún alumno.'});
+        }
+        res.render('equipo/equipo-nuevo', {
+            listaFormateadaAlumnos: req.body.valorTablaAlumnos,
+            listaFormateadaTemas: req.body.valorTablaTemas,
+            idgrupo: req.params.idgrupo,
+            errors
+        })
+    } else {
+        const equipo = await Models.equipo.create({
             nombre: nombre,
 
         })
     }
 
 }
+
+function tryParseJSON(jsonString) {
+    try {
+        let o = JSON.parse(jsonString);
+
+        // Handle non-exception-throwing cases:
+        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+        // but... JSON.parse(null) returns null, and typeof null === "object",
+        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+        if (o && typeof o === "object") {
+            return o;
+        }
+    } catch (e) {
+    }
+    return false;
+}
+
 module.exports = {
     renderAllTeams,
     renderNewTeam,
--- routes/equipo-rutas.js
+++ routes/equipo-rutas.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const equipoController = require('../controllers/equipo-controller');
 
-router.get('/grupo/equipos/:idgrupo',equipoController.renderAllTeams);
+router.get('/grupo/equipos/:idgrupo',equipoController.retriveTeamsData);
+router.get('/grupo/equipos/:idgrupo/:idequipo',equipoController.renderSelectedTeams);
+
 
 router.get('/equipo/nuevo/:idgrupo', equipoController.renderNewTeam);
 router.post('/equipo/nuevo/:idgrupo', equipoController.saveTeam)
