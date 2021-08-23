1c3036d9a357d1b6ca9235a7b27aa9f9cee776c8
Rodrigo Munguía <rmg.jp@outlook.com>
2021-08-23 18:18:20 -0500

    Agregadas entradas para visualización de segunda oportunidad.

--- routes/calificaciones-rutas.js
+++ routes/calificaciones-rutas.js
@@ -9,9 +9,12 @@ router.put('/actividad/calificar-individual/:idgrupo/:idtema/:idactividad', cali
 router.get('/actividad/calificar-individual/:idgrupo/:idtema/:idactividad/:second', calificacionController.renderViewCalif)
 
 router.put('/actividad/calificar-individual-segunda-oportunidad/:idgrupo/:idtema/:idactividad/:second', calificacionController.scoreSingle);
-
+//
 router.get('/grupo/calificaciones/:idgrupo', calificacionController.retriveCalf);
+//Originalmente esta ruta contenia el parametro modo, fue removido para la renderización de la vista de calificaciones de 2da oportunidad
+router.get('/grupo/calificaciones/:idgrupo/:idtema/', calificacionController.viewCalf);
 
+//Esta ruta ahora pertenece a la vista para vista de calificaciones de segunda oportunidad
 router.get('/grupo/calificaciones/:idgrupo/:idtema/:modo', calificacionController.viewCalf);
 
 router.get('/actividad/calificar-equipo/:idgrupo/:idtema/:idactividad/:idequipo', calificacionController.renderScoreTeam);
