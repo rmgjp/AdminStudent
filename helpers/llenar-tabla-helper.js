8c4ce2aa5e5a7946bc95ae315cf852e8fef52628
Rodrigo Munguía <rmg.jp@outlook.com>
2021-09-24 18:35:38 -0500

    Optimización de carga de datos.

--- helpers/llenar-tabla-helper.js
+++ helpers/llenar-tabla-helper.js
@@ -5,32 +5,15 @@ let i = 0;
 $(function cargarDatos() {
     var lista = document.getElementById("valorTabla").value;
     lista = JSON.parse(lista);
+    table.bootstrapTable('load', lista);
+    tablaDatos = table.bootstrapTable('getData');
+    document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
 
-    if (lista){
-        var rows = [];
-        for (let dato = 0; dato < lista.length; dato++) {
-            rows.push(lista[dato]);
-        }
-        table.bootstrapTable('load', rows);
-        i = lista.length;
-        tablaDatos = table.bootstrapTable('getData');
-        document.getElementById("valorTabla").value = JSON.stringify(tablaDatos);
-    }
-})
-
+});
 table.on('editable-save.bs.table', function (e, field, row, oldValue, $el) {
     tablaDatos = table.bootstrapTable('getData');
     document.getElementById("valorTabla").value = JSON.stringify(tablaDatos)
-    let $next = $(this).closest('tr').next().find('.editable');
-    setTimeout(function() {
-        $next.editable('show');
-    }, );
-    console.log($next);
 })
-
-
-
-
 
 
 
