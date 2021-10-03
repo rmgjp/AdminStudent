bd236949616f758ee6a0b03f83f76b97a05ec4b8
Rodrigo Munguía <rmg.jp@outlook.com>
2021-10-02 23:08:17 -0500

    El tipo de dato de valor ahora es double mejorando el calculo de calificaciones

--- models/actividad.js
+++ models/actividad.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     nombre: DataTypes.STRING,
       descripcion: DataTypes.STRING,
-      valor: DataTypes.INTEGER,
+      valor: DataTypes.DOUBLE,
       tipo: DataTypes.STRING
   },
       {
